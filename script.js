04d3788461b1561288db9146cca0baef9ea44296
Tyler <tstelly918@gmail.com>
2024-02-16 18:45:32 -0500

    making function use appropriately sized divs based on the size so it fits the screen

--- script.js
+++ script.js
@@ -1,28 +1,74 @@
+// DOM references
+let button = document.getElementById("userinput");
+const flexContainer = document.getElementById("MasterContainer"); // Make sure this ID matches your actual container
+let size = promptUser()
+// Global scope variables
+let rows;
+let columns;
 
-function draw(){
-const myDiv = document.createElement("div"); 
-myDiv.style.height = "100px";
-myDiv.style.width = "100px";
-myDiv.style.backgroundColor = "blue";
-myDiv.textContent = "hello poop people"
-document.body.appendChild(myDiv);
+// Main content
+function promptUser() {
+    let userInput = prompt("Enter two numbers of columns and rows separated by a comma");
+    // Split the input by comma and convert each element to a number
+    let num1 = Number(userInput.split(",")[0]);
+    let num2 = Number(userInput.split(",")[1]);
+    let userResult = num1 * num2;
+   console.log(userResult);
 }
 
-function makeGrids(size) {
-    const screen = document.querySelector(".sketch-screen");
-    for (let i = 0; i < size; i++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        for (let j = 1; j <= size; j++) {
-            const row = document.createElement("div");
-            row.classList.add("row");
-            row.innerText = i * size + j;
-            column.appendChild(row);
+function createGrid() {
+    if (size >= 0 && size <= 256) {
+        for (let i = 0; i < size; i++) {
+            const squares = document.createElement("div");
+            squares.classList = "flex-item-large";
+            flexContainer.appendChild(squares);
         }
-        screen.appendChild(column);
+    } else if (size >= 258 && size <= 784) {
+        for (let i = 0; i < size; i++) {
+            const squares = document.createElement("div");
+            squares.classList = "flex-item-med";
+            flexContainer.appendChild(squares);
+        }
+    } else if (size >= 786 && size <= 4096) {
+        for (let i = 0; i < size; i++) {
+            const squares = document.createElement("div");
+            squares.classList = "flex-item-small";
+            flexContainer.appendChild(squares);
+        }
+        // Customize the content or other properties of each square if needed
     }
 }
 
-makeGrids(16);
-draw();
+createGrid(size);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
